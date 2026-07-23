5f69ac65d8733ab4eb214dfa39fae4627a8d18ba
Angelo Roberto Da Silva Medeiros <angelo.roberto.medeiros@gmail.com>
2026-07-23 15:48:14 -0300

    0.1
    primeira versão construída 0.1

---
+++ Lista_De_Compras/src/Produto.java
@@ -0,0 +1,30 @@
+public class Produto {
+    private String nomeProduto;
+    private String categoria;
+    private String seuNome;
+
+
+    public String getNomeProduto() {
+        return nomeProduto;
+    }
+
+    public void setNomeProduto(String nomeProduto) {
+        this.nomeProduto = nomeProduto;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public String getSeuNome() {
+        return seuNome;
+    }
+
+    public void setSeuNome(String seuNome) {
+        this.seuNome = seuNome;
+    }
+}
