b95918f6c83d30a04bdafbabb491a755e6db9d6e
Angelo <angelo.roberto.medeiros@gmail.com>
2026-08-12 19:32:33 -0300

    main corrigido

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class Main {
-    static void main() throws IOException {
+    static void main(String[] args) throws IOException {
         try (FileWriter writer = new FileWriter("produtos.csv", true)) {}
         Menus menu = new Menus("produtos.csv");
         ServidorWeb servidor = new ServidorWeb(menu);
